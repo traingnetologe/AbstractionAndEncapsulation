233b8d87f1e2b046a68f82c27a3d362dde7a0ba3
26taivanovsergey <ta.ivanovsergey@gmail.com>
2024-11-19 11:18:37 +0300

    init commit

---
+++ address_sort/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <locale>
+// подключаем чтобы были быстрые сортировки, что бы не писать самостоятельно
+#include <algorithm>
+
+// Комментарии пишу для себя что бы было понятно что где происходит.
+// Спасибо большое заранее.
+class Address{
+
+private:
+std::string citi{}, street{};
+int house{}, apartment{};
+
+public:
+// Конструкторы класс
+    Address() {}
+    Address(std::string set_citi, std::string set_street, int set_house, int set_appartment): citi(set_citi), street(set_street), house(set_house), apartment(set_appartment){ }
+
+std::string get_stirng_Address(){
+    return citi + ", " + street + ", " + std::to_string(house) + ", " + std::to_string(apartment)+ "\n";
+}
+
+std::string get_siti(){
+    return citi;
+}
+};
+// Пока я размечтался что у меня есть компоратор который может сравнивать строки
+int Comparison_Class_Address(Address* oneAddress, Address* twoAddress){
+    return oneAddress->get_stirng_Address().compare(twoAddress->get_stirng_Address());
+}
+int main() {
+
+setlocale(LC_ALL, "Russian");
+
+// to do Объявление константных переменных
+const std::string PATH_FILE_SOUCE= "./source_text.txt";
+const std::string PATH_FILE_OUT = "./out.txt";
+const int SIZI_STRING_CONTAINER = 3;
+
+// блок чтения из файла
+std::ifstream fin(PATH_FILE_SOUCE);
+int cout_container;
+fin >> cout_container;
+// std::cout << cout_container;
+
+std::vector <Address*> ContainerAddress;
+
+// Блок объявление временных переменных
+std::string set_citi{}, set_street{};
+int set_house{}, set_appartment{};
+
+// Считываем данные из файла во временные переменые и сразу конструируем объекты
+for (int i = 0; i < cout_container; i++)
+{
+    fin >> set_citi; fin >> set_street; fin >> set_house; fin >> set_appartment;
+    ContainerAddress.push_back(new Address(set_citi, set_street, set_house, set_appartment));
+}
+
+// Закрываем ранее открытый файл
+fin.close();
+// sorted vector for citi
+
+std::sort(ContainerAddress.begin(), ContainerAddress.end(), [](Address* oneObject, Address* towObject){
+    return oneObject->get_siti() > towObject->get_siti();
+});
+
+// Записывае данные в файл
+std::ofstream fop(PATH_FILE_OUT);
+fop << cout_container << "\n";
+
+for (int i = ContainerAddress.size()-1; i >= 0; i--)
+{
+    fop << ContainerAddress[i]->get_stirng_Address();
+    // std::cout << ContainerAddress[i]->get_stirng_Address()<< std::endl;
+}
+
+// for (int i = 0; i < ContainerAddress.size(); i++)
+// {
+//     fop << ContainerAddress[i]->get_stirng_Address();
+//     std::cout << ContainerAddress[i]->get_stirng_Address();
+// }
+fop.close();
+
+// Освобождаем ранее занятую память
+for (int i = 0; i < ContainerAddress.size(); i++)
+{
+    delete ContainerAddress[i];
+}
+
+return 0;
+}
